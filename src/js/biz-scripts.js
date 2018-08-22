35a8d0fcf4934b5e20061853709f62438c5b4ac3
Nikhil <nikhil@coniferlabs.in>
2018-08-22 12:46:58 +0530

    form-validation

--- src/js/biz-scripts.js
+++ src/js/biz-scripts.js
@@ -1,66 +1,65 @@
+$(document).ready(function() {
 
-$(document).ready(function(){
+    // biz slider
 
-  // biz slider
-
-   $('.owl-carousel').owlCarousel({
+    $('.owl-carousel').owlCarousel({
 
         autoplay: true,
-        loop:true,
-        responsiveClass:true,
-        responsive:{
-            0:{
-                items:1,
-                nav:true
+        loop: true,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
             },
-            600:{
-                items:2,
-                nav:false
+            600: {
+                items: 2,
+                nav: false
             },
-            1000:{
-                items:2,
-                nav:true,
-                loop:true
+            1000: {
+                items: 2,
+                nav: true,
+                loop: true
             }
         }
     });
 
-   // Includin header & footer
+    // Includin header & footer
 
-   //$('#header-new').load("include/header.html");    
+    //$('#header-new').load("include/header.html");    
 
-   $('#header-new').load("include/header.html", function() {
-        
-      path_class();
+    $('#header-new').load("include/header.html", function() {
+
+        path_class();
 
     });
 
-   $('#new-footer').load("include/footer.html", function(){
+    $('#new-footer').load("include/footer.html", function() {
 
-      form_validation();
+        form_validation();
 
-   });
+    });
 
 
-   // menu toggle
+    // menu toggle
 
-   $(".mb-nav").click(function(){
+    $(".mb-nav").click(function() {
 
-      $(".main-nav").toggle();
+        $(".main-nav").toggle();
 
-      $(this).toggleClass("rot");
+        $(this).toggleClass("rot");
 
-      $("#main-menu-overlay").toggleClass("o-active");
-      
-   });
+        $("#main-menu-overlay").toggleClass("o-active");
 
-   // custom swipe section
+    });
 
-   // swipe 1
+    // custom swipe section
 
-     $('.swipe-1 .nav-tabs li').click( function() {
+    // swipe 1
 
-        $(this).addClass('active').siblings().removeClass('active');  
+    $('.swipe-1 .nav-tabs li').click(function() {
+
+        $(this).addClass('active').siblings().removeClass('active');
 
         var attr = $(this).children('a').attr("data-roles");
 
@@ -68,19 +67,19 @@ $(document).ready(function(){
 
         $(".swipe-1 .tab-content .tab-pane").removeClass("active");
 
-        $(".swipe-1 .tab-content").find('#' + attr).addClass("active");  
+        $(".swipe-1 .tab-content").find('#' + attr).addClass("active");
 
     });
 
-     $(".swipe-1 .card").swipe( {
+    $(".swipe-1 .card").swipe({
 
-       swipeLeft:function(event, direction, distance, duration, fingerCount) {
+        swipeLeft: function(event, direction, distance, duration, fingerCount) {
 
             $(".swipe-1 .nav-tabs li.active").next('li').addClass("active");
 
             $(".swipe-1 .nav-tabs li.active").prev('li').removeClass("active");
 
-            var newText = $('.nav-tabs li.active');  
+            var newText = $('.nav-tabs li.active');
 
             var attr = newText.children('a').attr("data-roles");
 
@@ -88,17 +87,17 @@ $(document).ready(function(){
 
             $(".swipe-1 .tab-content .tab-pane").removeClass("active");
 
-            $(".swipe-1 .tab-content").find('#' + attr).addClass("active");  
-          
-          },
+            $(".swipe-1 .tab-content").find('#' + attr).addClass("active");
 
-       swipeRight:function(event, direction, distance, duration, fingerCount) {
+        },
 
-          $(".swipe-1 .nav-tabs li.active").prev('li').addClass("active");
+        swipeRight: function(event, direction, distance, duration, fingerCount) {
+
+            $(".swipe-1 .nav-tabs li.active").prev('li').addClass("active");
 
             $(".swipe-1 .nav-tabs li.active").next('li').removeClass("active");
 
-            var newText = $('.nav-tabs li.active');  
+            var newText = $('.nav-tabs li.active');
 
             var attr = newText.children('a').attr("data-roles");
 
@@ -106,17 +105,17 @@ $(document).ready(function(){
 
             $(".swipe-1 .tab-content .tab-pane").removeClass("active");
 
-            $(".swipe-1 .tab-content").find('#' + attr).addClass("active");  
-       
+            $(".swipe-1 .tab-content").find('#' + attr).addClass("active");
+
         }
 
     });
 
-     // swipe-2
+    // swipe-2
 
-      $('.swipe-2 .nav-tabs li').click( function() {
+    $('.swipe-2 .nav-tabs li').click(function() {
 
-        $(this).addClass('active').siblings().removeClass('active');  
+        $(this).addClass('active').siblings().removeClass('active');
 
         var attr = $(this).children('a').attr("data-roles");
 
@@ -124,19 +123,19 @@ $(document).ready(function(){
 
         $(".swipe-2 .tab-content .tab-pane").removeClass("active");
 
-        $(".swipe-2 .tab-content").find('#' + attr).addClass("active");  
+        $(".swipe-2 .tab-content").find('#' + attr).addClass("active");
 
     });
 
-     $(".swipe-2 .card").swipe( {
+    $(".swipe-2 .card").swipe({
 
-       swipeLeft:function(event, direction, distance, duration, fingerCount) {
+        swipeLeft: function(event, direction, distance, duration, fingerCount) {
 
             $(".swipe-2 .nav-tabs li.active").next('li').addClass("active");
 
             $(".swipe-2 .nav-tabs li.active").prev('li').removeClass("active");
 
-            var newText = $('.swipe-2 .nav-tabs li.active');  
+            var newText = $('.swipe-2 .nav-tabs li.active');
 
             var attr = newText.children('a').attr("data-roles");
 
@@ -144,17 +143,17 @@ $(document).ready(function(){
 
             $(".swipe-2 .tab-content .tab-pane").removeClass("active");
 
-            $(".swipe-2 .tab-content").find('#' + attr).addClass("active");  
-          
-          },
+            $(".swipe-2 .tab-content").find('#' + attr).addClass("active");
 
-       swipeRight:function(event, direction, distance, duration, fingerCount) {
+        },
 
-          $(".swipe-2 .nav-tabs li.active").prev('li').addClass("active");
+        swipeRight: function(event, direction, distance, duration, fingerCount) {
+
+            $(".swipe-2 .nav-tabs li.active").prev('li').addClass("active");
 
             $(".swipe-2 .nav-tabs li.active").next('li').removeClass("active");
 
-            var newText = $('.swipe-2 .nav-tabs li.active');  
+            var newText = $('.swipe-2 .nav-tabs li.active');
 
             var attr = newText.children('a').attr("data-roles");
 
@@ -162,8 +161,8 @@ $(document).ready(function(){
 
             $(".swipe-2 .tab-content .tab-pane").removeClass("active");
 
-            $(".swipe-2 .tab-content").find('#' + attr).addClass("active");  
-       
+            $(".swipe-2 .tab-content").find('#' + attr).addClass("active");
+
         }
 
     });
@@ -171,51 +170,92 @@ $(document).ready(function(){
 
     // pricing accordian mobile
 
-    $('.detail-view').click(function(){
+    $('.detail-view').click(function() {
 
         $(this).toggleClass('detail-active');
 
         $(this).siblings().closest('.card-detail').fadeToggle('card-active');
-        
+
     });
 
+    // $('.form-sec a').click(function() {
+    //   console.log('button clicked')
+    //     $.ajax({
+    //         type: "POST",
+    //         url: 'https://biz.dev.coniferlabs.in:4001/service/contactUs',
+    //         // headers: {
+    //         //     "x-access-token": token,
+    //         // },
+    //           data: { 'name': 'nikhil',
+    //                   'email': 'nikhils9it@gmail.com',
+    //                   'message': 'Testing'
+    //                 },
+    //         success: function(res) {
+    //             console.log(res)
+    //         },
+    //         error: function(error) {
+    //             console.log(error)
+    //         }
+    //     });
+
+    // });
 
 }); // document ready function
+
+  function clicked(){
+     
+        $.ajax({
+            type: "POST",
+            url: 'https://biz.dev.coniferlabs.in:4001/service/contactUs',
+           
+              data: { 'name': $(".fullname").val(),
+                      'email': $(".email").val(),
+                      'message': $(".shortmsg").val()
+                    },
+
+            success: function(res) {
+                $('.form-sec input[type="text"],texatrea').val('');
+                $('<div class="success">Your Details has been submitted successfully</div>').insertAfter('.shortmsg').delay(4000).fadeOut();
+            },
+            error: function(error) {
+                $('<div class="form-error">Your Details could not be submitted</div>').insertAfter('.shortmsg').delay(4000).fadeOut();
+            }
+        })
+      };
 
 
 // functions
 
 function path_class() {
 
-var path = window.location.pathname.split("/").pop();
-  
-      // Account for home page with empty path
-      if ( path == '' ) {
+    var path = window.location.pathname.split("/").pop();
+
+    // Account for home page with empty path
+    if (path == '') {
         path = 'index.php';
-      }
-          
-      var target = $('nav a[href="'+path+'"]');
-      // Add active class to target link
-      target.addClass('active-menu');
+    }
+
+    var target = $('nav a[href="' + path + '"]');
+    // Add active class to target link
+    target.addClass('active-menu');
 }
 
 function form_validation() {
 
-  $(".submit-btn").on("click", function(){
+    $(".submit-btn").on("click", function() {
 
-        $('input').each(function(){
-            if($(this).attr('isrequired') == 'yes'){
-            var inputName = $(this).attr('class');
-            var inputVal = $(this).val();
-            var errorMsg = $(this).attr('data-msg');
-            if(inputVal == '' && !$('.error').hasClass(inputName+'error')){
-            $('<div class="error '+inputName+'error">'+errorMsg+'</div>').insertAfter(this);
-            }else{
-            $('.'+inputName+'error').remove();
-            }
+        $('input').each(function() {
+            if ($(this).attr('isrequired') == 'yes') {
+                var inputName = $(this).attr('class');
+                var inputVal = $(this).val();
+                var errorMsg = $(this).attr('data-msg');
+                if (inputVal == '' && !$('.error').hasClass(inputName + 'error')) {
+                    $('<div class="error ' + inputName + 'error">' + errorMsg + '</div>').insertAfter(this);
+                } else {
+                    $('.' + inputName + 'error').remove();
+                }
             }
         })
 
-  });
+    });
 }
-
